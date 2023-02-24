d756a98e7e6f7df7092f51e0a87051462da4324f
kaloyanpepelyashki <kaloyan.pepelyashki@gmail.com>
2023-02-24 18:09:54 +0000

    Update LandingPage.jsx

--- Client/src/Pages/Global pages/LandingPage.jsx
+++ Client/src/Pages/Global pages/LandingPage.jsx
@@ -9,7 +9,7 @@ export default function LandingPage() {
             Before starting your shop journey, do you want us to sed you
             <b className="text-primary-color">our catalog</b>?
           </h1>
-          <a href="mailto:kaloyan.professional@gmail.com?subject={subject}&body={body}">
+          <a href="mailto:kaloyan.professional@gmail.com?subject=`{subject}`&body=`{body}`">
             Click to Send an Email
           </a>
         </div>
